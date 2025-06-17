4bad4f7df92debee59f8f7730e923b6d091c621a
Priya <ganapathipriyadharsani@gmail.com>
2025-06-17 14:57:27 +0530

    add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add this part for images
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.openfoodfacts.org',
+        port: '',
+        pathname: '/images/products/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
